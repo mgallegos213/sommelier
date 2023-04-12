75abcaf75ec5720da70cffa3ce02887dff1c58fe
Michael Gallegos <michael@mgallegos.dev>
2023-04-12 07:43:42 -0500

    feat: convert and encode cosmos address - not yet fully working
    Does not have parity with Ian Coleman Bip39 tool that derives
    addresses from a seed/phrase.

--- generate-key/generate-phrase.go
+++ generate-key/generate-phrase.go
@@ -1,40 +1,67 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
-
-	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/cosmos/go-bip39"
+	bech32_btcsuite "github.com/btcsuite/btcutil/bech32"
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	bech32_cosmos "github.com/cosmos/cosmos-sdk/types/bech32"
+	"github.com/tendermint/tendermint/crypto/secp256k1"
+	"log"
 )
 
 func main() {
 	// Generate a mnemonic for memorization or user-friendly seeds
-	entropy, _ := bip39.NewEntropy(256)
-	mnemonic, _ := bip39.NewMnemonic(entropy)
+	//entropy, _ := bip39.NewEntropy(256)
+	//mnemonic, _ := bip39.NewMnemonic(entropy)
+	//
+	//fmt.Println("phrase: ", mnemonic)
 
-	// Generate the seed phrase encoded by a secret passphrase
-	seed := bip39.NewSeed(mnemonic, "Secret Passphrase")
+	//// Generate the seed phrase encoded by a secret passphrase
+	//seed := bip39.NewSeed(mnemonic, "") // siege drastic unique edit crane east real taxi excuse market lonely cradle second rug mail pig still already poverty unfair approve salt symptom reward
+	seed, _ := hex.DecodeString("c476c8378c2aa3ac0c6b91d5f6e32e1fcf1c66898564a77b182f11fa8481f635f38e988d767fc244157762afbf954afe56d21b24310d855cefe131b8c647c3fb")
+	fmt.Println("seed: ", hex.EncodeToString(seed))
 
-	fmt.Println("phrase: ", mnemonic)
-	fmt.Println("seed: ", seed)
+	master, ch := hd.ComputeMastersFromSeed(seed)
+	path := "m/44'/118'/0'/0/0'"
+	priv, err := hd.DerivePrivateKeyForPath(master, ch, path) // Note: is this where the discrepancy is originating?
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Derivation Path: ", path)                 // Derivation Path:  m/44'/118'/0'/0/0'
+	fmt.Println("Private Key: ", hex.EncodeToString(priv)) // Private Key:  873448e11b53bb18ef74c27a66f033b85aff6d494377d088f2915c216665d4a4
 
-	// Generate a keyring from the seed phrase, store in memory for now
-	// cdc := codec.Codec
-	// keyringBackend := keyring.NewInMemory()
-	// info, err := keyringBackend.NewAccount("accname", string(seed), "", hd.CreateHDPath(118, 0, 0))
-	// if err != nil {
-	// 	panic(err)
-	// }
+	var privKey = secp256k1.PrivKey(priv)
+	pubKey := privKey.PubKey()
+	pubKeyBytes := pubKey.Bytes()
+	pubKeyAddress := pubKey.Address()
+	fmt.Println("Pub key address:", pubKeyAddress)               // 5DCCB46060EC0982504401368E0636B447241F68
+	fmt.Println("Public Key: ", hex.EncodeToString(pubKeyBytes)) // Public Key:  0325fc1bf2ba8e48ea470980e65a2ef2747dd441097ab104cff90b8d4819bb1fb2
 
-	// fmt.Println(info)
+	// Method A -> BTCSuite dependency
+	// Convert byte slice to Bech32 string
+	bech32bits, err := bech32_btcsuite.ConvertBits(pubKeyAddress.Bytes(), 8, 5, true)
+	if err != nil {
+		panic(err)
+	}
 
-	// // Print the first 5 child addresses with their HD paths
-	// for i := 0; i < 5; i++ {
-	// 	path := hd.CreateHDPath(118, 0, uint32(i))
-	// 	child, err := hd.DeriveFn(mnemonic, "", path)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	fmt.Println("Address:", child.GetAddress().String(), "HD path:", path.String())
-	// }
+	bech32encodedA, _ := bech32_btcsuite.Encode("cosmos", bech32bits)
+	if err != nil {
+		panic(err)
+	}
+	// ====================
+	// Method B -> Cosmos library dependency
+	// Print Bech32-encoded string
+	bech32encodedB, _ := bech32_cosmos.ConvertAndEncode("cosmos", pubKeyAddress.Bytes())
+	if err != nil {
+		panic(err)
+	}
+	// =============
+	// Note: methods A and B produce the same output at the moment.
+	// Nice to ensure parity, but it's still not quite what it should be.
+	fmt.Println("prefixed encoded A: ", bech32encodedA) // should be: cosmos1gk49nd4zearkf22gven8eyacvrspccr7d0xtkm
+	// currently getting: cosmos1thxtgcrqasycy5zyqymgup3kk3rjg8mgupcz6k
+
+	fmt.Println("prefixed encoded B: ", bech32encodedB) // should be: cosmos1gk49nd4zearkf22gven8eyacvrspccr7d0xtkm
+	// currently getting: cosmos1thxtgcrqasycy5zyqymgup3kk3rjg8mgupcz6k
 }
--- generate-key/go.mod
+++ generate-key/go.mod
@@ -2,9 +2,39 @@ module generate-phrase
 
 go 1.20
 
-require github.com/cosmos/go-bip39 v1.0.0
+require (
+	github.com/btcsuite/btcutil v1.0.3-0.20201208143702-a53e38424cce
+	github.com/cosmos/cosmos-sdk v0.47.1
+	github.com/decred/dcrd/bech32 v1.1.2
+	github.com/tendermint/tendermint v0.35.9
+)
 
 require (
-	github.com/stretchr/testify v1.8.2 // indirect
+	cosmossdk.io/errors v1.0.0-beta.7 // indirect
+	github.com/btcsuite/btcd v0.22.1 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/cometbft/cometbft v0.37.0 // indirect
+	github.com/cosmos/btcutil v1.0.5 // indirect
+	github.com/cosmos/go-bip39 v1.0.0 // indirect
+	github.com/cosmos/gogoproto v1.4.6 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/go-kit/log v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sasha-s/go-deadlock v0.3.1 // indirect
+	github.com/tendermint/go-amino v0.16.0 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/genproto v0.0.0-20230216225411-c8e22ba71e44 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	google.golang.org/protobuf v1.29.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
 )
